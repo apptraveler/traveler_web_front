c9918ec8fb4f057414f5fb76de3db96eba080a0c
igoohd <igor.duartematos@gmail.com>
2022-09-20 20:15:52 -0300

    feat: Start form validations

--- src/components/AuthInfo/ForgotPassword/index.tsx
+++ src/components/AuthInfo/ForgotPassword/index.tsx
@@ -2,16 +2,33 @@ import { StepBackwardIcon, TextInputField, Button, LogInIcon } from 'evergreen-u
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import classes from './index.module.scss'
+import { useForm } from "react-hook-form";
+import { validateEmail } from "@utils/validators"
 
 function ForgotPassword() {
-  const [value, setValue] = React.useState('')
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const [formData, setFormData] = React.useState({
+    email: ''
+  })
+
+  const onSubmit = (data: any) => {
+    setFormData(data)
+  };
+
+  console.log(watch("email")); 
 
   return (
-    <div className={classes['forgot-password']}>
+    <form
+      className={classes['forgot-password']}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <TextInputField
+        className="teste -form"
         label="E-mail"
         placeholder="Digite seu e-mail"
         required
+        isInvalid={validateEmail(formData.email)}
+        {...register("email", { required: true })}
       />
       <Button
         width={'100%'}
@@ -19,12 +36,13 @@ function ForgotPassword() {
         marginRight={12}
         iconAfter={LogInIcon}
         appearance='primary'
+        type="submit"
       >
         Recuperar Senha
       </Button>
+      <input type="submit" />
       <Button
         is={RouterLink}
-        width={'100%'}
         marginY={8} 
         marginRight={12}
         iconBefore={StepBackwardIcon}
@@ -33,7 +51,7 @@ function ForgotPassword() {
       >
         Voltar
       </Button>
-    </div>
+    </form>
   )
 }
 
