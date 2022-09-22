6fca4ef380ee1d5e2eee59bbcf097e43a8ea0bdc
igoohd <igor.duartematos@gmail.com>
2022-09-22 15:22:31 -0300

    feat: Start validation SignUp

--- src/components/AuthInfo/SignUp/index.tsx
+++ src/components/AuthInfo/SignUp/index.tsx
@@ -1,30 +1,60 @@
 import { TextInputField, Button, NewPersonIcon } from 'evergreen-ui'
 import React from 'react'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { authSchema } from '@utils/validation'
 
 function SignIn() {
-  const [value, setValue] = React.useState('')
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(authSchema)
+  });
+
+  const [formData, setFormData] = React.useState({
+    email: ''
+  })
+
+  const [submitError, setSubmitError] = React.useState(false)
+
+  const onSubmit = (data: any) => {
+    setFormData(data)
+    setSubmitError(true)
+  };
 
   return (
-    <div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <TextInputField
         label="Nome"
         placeholder="Digite seu nome"
         required
+        isInvalid={!!errors.name}
+        validationMessage={!!errors.name ? errors.name.message : undefined}
+        {...register("name")}
       />
       <TextInputField
         label="E-mail"
         placeholder="Digite seu e-mail"
         required
+        isInvalid={!!errors.email}
+        validationMessage={!!errors.email ? errors.email.message : undefined}
+        {...register("email")}
       />
       <TextInputField
         label="Senha"
         placeholder="Digite sua senha"
         required
+        isInvalid={!!errors.password}
+        validationMessage={!!errors.password ? errors.password.message : undefined}
+        {...register("password")}
       />
       <TextInputField
         label="Confirmação de senha"
         placeholder="Confirme sua senha"
         required
+        isInvalid={!!errors.confirmPassword}
+        validationMessage={!!errors.confirmPassword ? errors.confirmPassword.message : undefined}
+        {...register("confirmPassword")}
       />
       <Button
         width={'100%'}
@@ -35,7 +65,7 @@ function SignIn() {
       >
         Cadastrar
       </Button>
-    </div>
+    </form>
   )
 }
 
