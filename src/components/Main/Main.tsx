d9e28ca089bf35550881c4f8b5125c57cc8f434e
igoohd <igor.duartematos@gmail.com>
2022-10-06 12:06:41 -0300

    feat: Start structure Dashboard page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Auth from '@pages/Auth';
 import ProfileForm from '@pages/ProfileForm';
 import NotFound from '@pages/NotFound';
+import Dashboard from '@pages/Dashboard';
 
 const Main = () => {
   return (
     <Routes>
       <Route path='/auth' element={<Auth/>}></Route>
       <Route path='/forgot-password' element={<Auth/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
       <Route path='/profile-form' element={<ProfileForm/>}></Route>
       <Route path='*' element={<NotFound/>}></Route>
     </Routes>
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Pane, Paragraph } from 'evergreen-ui';
+import classes from './index.module.scss'
+const Dashboard = () => {
+  return (
+    <Pane className={classes['dashboard-page']}>
+    </Pane>
+  )
+}
+
+export default Dashboard;
